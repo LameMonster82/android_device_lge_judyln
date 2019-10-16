24157323eaa754b44874c724f286a20d8662590c
Jake Weinstein <jake@aospa.co>
2019-10-16 07:23:30 +0200

    sdm845-common: configure optimal cpusets
    These provide the best balance of performance and power on SDM845
    
    Change-Id: I49261089163847fd0ff929475de8ad2a31e6c90a
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>
    Signed-off-by: SGCMarkus <markusornik@gmail.com>

--- rootdir/bin/init.qcom.post_boot.sh
+++ rootdir/bin/init.qcom.post_boot.sh
@@ -3666,7 +3666,7 @@ case "$target" in
 	# cpuset parameters
 #        echo 0-3 > /dev/cpuset/background/cpus
         echo 0-1 > /dev/cpuset/background/cpus
-        echo 0-3 > /dev/cpuset/system-background/cpus
+        echo 0-2 > /dev/cpuset/system-background/cpus
         echo 0-3 > /dev/cpuset/restricted/cpus
         echo 0-3,6-7 > /dev/cpuset/foreground/cpus
 
