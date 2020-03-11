d23a042b6233eff2d1adacdfdf0746584b4f2bd0
SGCMarkus <markusornik@gmail.com>
2020-03-11 21:04:40 +0100

    judypn: add device specidifc props for RIL and camera

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -2,4 +2,14 @@
 # vendor props for judypn
 #
 
+# Camera
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.lge.aux.camera=2 \
+    vendor.lge.faceprint.use.camera=0
 
+# Misc
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.lge.build.target_country=COM \
+    ro.vendor.lge.build.target_operator=LAO \
+    ro.vendor.lge.capp_cupss.rootdir=/oem/OP \
+    ro.vendor.lge.swversion=V40520e
