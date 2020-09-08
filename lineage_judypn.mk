5b229a50153aa4fbc153d38ff68045a56f51b893
SGCMarkus <markusornik@gmail.com>
2020-09-08 12:31:23 +0200

    judypn: increment launched with to pie
    this allows us to not use SELINUX_IGNORE_NEVERALLOWS as the fingerprint service was the only conflict
    would have needed following rules:
    allow hal_fingerprint_default fingerprintd_data_file:dir rw_dir_perms;
    allow hal_fingerprint_default fingerprintd_data_file:file rw_file_perms;
    allow tee fingerprintd_data_file:dir rw_dir_perms;
    allow tee fingerprintd_data_file:file rw_file_perms;

--- lineage_judypn.mk
+++ lineage_judypn.mk
@@ -18,7 +18,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
 # Installs gsi keys into ramdisk, to boot a GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
